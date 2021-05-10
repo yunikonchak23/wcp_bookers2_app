746389301455d1df110e1609abe2a180f1b3b2c4
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-10 10:57:18 +0000

    [Add]profile-img

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ac6b5358ed153400c946699f60287a2cc129bb69201344b6ffdc5d220cefdaa1133cb13d1d8aabcb22618354ace01bf9f10d0df6707077a99d6ee418debc1806'
